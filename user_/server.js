6f8b2426ebdb812a77d9bc64411a3581ceca3557
hyunseo <80755341+hyunseo1206@users.noreply.github.com>
2023-02-02 21:41:06 +0900

    Add files via upload
    회원가입, 로그인, 비밀번호 암호화 완료

---
+++ user_/server.js
@@ -0,0 +1,111 @@
+const express = require('express'); // express 임포트
+const app = express(); // app생성
+const bodyParser = require('body-parser');
+const port = 5000;
+const { User } = require('./models/User'); 
+const { auth } = require("./middleware/auth");
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencode
+app.use(express.json()); // application/json
+
+
+// 몽구스 연결
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    'mongodb+srv://lovelykitty0520:ysn09016@cluster0.eaz2vis.mongodb.net/snowvillage',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB connected!!'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// 1.
+app.get('/', function (req, res) {
+  res.json('정상적으로 연결되었습니다.');
+});
+
+// 2. register api
+// 회원가입에 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 저장해야 함.
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
+
+// 3. login api
+
+app.post("/login", (req, res) => {
+  //로그인을할때 아이디와 비밀번호를 받는다
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.json({
+        loginSuccess: false,
+        message: "존재하지 않는 아이디입니다.",
+      });
+    }
+    user
+      .comparePassword(req.body.password)
+      .then((isMatch) => {
+        if (!isMatch) {
+          return res.json({
+            loginSuccess: false,
+            message: "비밀번호가 일치하지 않습니다",
+          });
+        }
+        //비밀번호가 일치하면 토큰을 생성한다
+        //해야될것: jwt 토큰 생성하는 메소드 작성
+        user
+          .generateToken()
+          .then((user) => {
+            res.cookie("x_auth", user.token).status(200).json({
+              loginSuccess: true,
+              userId: user._id,
+            });
+          })
+          .catch((err) => {
+            res.status(400).send(err);
+          });
+      })``
+      .catch((err) => res.json({ loginSuccess: false, err }));
+  });
+});
+
+//auth 미들웨어를 가져온다
+//auth 미들웨어에서 필요한것 : Token을 찾아서 검증하기
+app.get("/auth", auth, (req, res) => {
+  //auth 미들웨어를 통과한 상태 이므로
+  //req.user에 user값을 넣어줬으므로
+  res.status(200).json({
+    _id: req._id,
+    isAdmin: req.user.role === 09 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+  
